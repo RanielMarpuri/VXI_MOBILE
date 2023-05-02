b353fc3d5c7db9a8c33180a0ff06248be722d09e
RanielMarpuri <rdnmarps03@gmail.com>
2023-05-03 06:08:52 +0800

    added loader 2

--- src/app/pages/profile/modules/messages/messages.page.ts
+++ src/app/pages/profile/modules/messages/messages.page.ts
@@ -19,14 +19,14 @@ export class MessagesPage implements OnInit {
     {
       ID: '3',
       Subject: 'Mental Health Concerns',
-      CreatedBy: '4225726',
+      CreatedBy: '',
       CreatedAt: '04/15/2023',
       Status: 'open',
       messages: [
         {
           ID: '1',
           isRead: 1,
-          from: '4225726',
+          from: '',
           to: 'HR',
           Body: 'Dear HR,\nI would like to inform you, that I am no longer fit to work in this environment for I am concerned with my mental health.\nThank you,',
           MessageID: '3',
@@ -36,7 +36,7 @@ export class MessagesPage implements OnInit {
           ID: '2',
           isRead: 1,
           from: 'HR',
-          to: '4225726',
+          to: '',
           Body: 'Dear ma\'am/sir,\nThank you for reaching out, may we ask a particular incident sparking your decision to declare not-fit-to-work?\nWe value our employees and would like to hear more about the issue.\nRegards,',
           MessageID: '3',
           CreatedAt: '04/16/2023',
@@ -44,7 +44,7 @@ export class MessagesPage implements OnInit {
         {
           ID: '3',
           isRead: 1,
-          from: '4225726',
+          from: '',
           to: 'HR',
           Body: 'Yes. But can I discuss this further in your office maam.\nThank you.',
           MessageID: '3',
@@ -54,7 +54,7 @@ export class MessagesPage implements OnInit {
           ID: '4',
           isRead: 0,
           from: 'HR',
-          to: '4225726',
+          to: '',
           Body: 'Anytime, and please always remember, you are not alone.\nThank you.',
           MessageID: '3',
           CreatedAt: '04/17/2023',
@@ -64,7 +64,7 @@ export class MessagesPage implements OnInit {
     {
       ID: '2',
       Subject: 'Welcome to VXI!',
-      CreatedBy: '4225726',
+      CreatedBy: '',
       CreatedAt: '04/14/2023',
       Status: 'open',
       messages: [
@@ -72,7 +72,7 @@ export class MessagesPage implements OnInit {
           ID: '1',
           isRead: 1,
           from: 'HR',
-          to: '4225726',
+          to: '',
           Body: 'Welcome to VXI ma\'am/sir,\nWe would like to congratulate you on your new journey! And we wish you luck on your past endeavors.',
           MessageID: '2',
           CreatedAt: '04/14/2023'
@@ -82,7 +82,7 @@ export class MessagesPage implements OnInit {
     {
       ID: '1',
       Subject: 'Interview - Feb 18, 2023',
-      CreatedBy: '4225726',
+      CreatedBy: '',
       CreatedAt: '02/10/2023',
       Status: 'resolved',
       messages: [
@@ -90,7 +90,7 @@ export class MessagesPage implements OnInit {
           ID: '1',
           isRead: 1,
           from: 'HR',
-          to: '4225726',
+          to: '',
           Body: 'Welcome to VXI ma\'am/sir,\nWe would like to invite to a scheduled face-to-face interview this February 18, 2023.\nThank you,',
           MessageID: '1',
           CreatedAt: '02/10/2023'
@@ -127,7 +127,7 @@ export class MessagesPage implements OnInit {
     {
       ID: '1',
       isRead: 1,
-      from: '4225726',
+      from: '',
       to: 'HR',
       Body: this.reply_msg,
       MessageID: '2',
@@ -146,7 +146,7 @@ export class MessagesPage implements OnInit {
     let msg = {
       ID: '10',
       Subject: this.subject_line,
-      CreatedBy: '4225726',
+      CreatedBy: '',
       CreatedAt: '04/15/2023',
       Status: 'open',
       messages: []
@@ -155,7 +155,7 @@ export class MessagesPage implements OnInit {
     msg.messages.push({
       ID: '15',
       isRead: 1,
-      from: '4225726',
+      from: '',
       to: 'HR',
       Body: this.body_line,
       MessageID: '10',
